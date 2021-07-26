20d2274a367e7bd63b751e6e10faf4804a5c0ecd
DYMRIT <homadmitry2001@gmail.com>
2021-07-26 18:36:04 +0300

    Change for media files

--- BRAINSKILL/settings/production.py
+++ BRAINSKILL/settings/production.py
@@ -127,12 +127,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_DIRS = [
     BASE_DIR / "courses/static",
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
--- BRAINSKILL/urls.py
+++ BRAINSKILL/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('courses.urls', 'courses'))),
     path('accounts/', include(('accounts.urls', 'accounts'))),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
